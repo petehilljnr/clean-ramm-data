7d8b27090e4b987a16d9e2058e196ee6415e05bc
petehilljnr <peter.hill@mwhglobal.com>
2015-09-10 16:50:17 +1200

    moved to list - better test files

--- clean.py
+++ clean.py
@@ -25,16 +25,22 @@ data = list(infile)
 
 line_num = 1
 
+print data
+data[2:4] = [''.join(data[2:4])]
+
+print data
+
 for line in data:
+	
     if line.count(delimiter) == num_delim:
         #correct number
+        print line
         outfile.write(line)
 
-    #elif line.count(delimiter) < num_delim:
+    elif line.count(delimiter) < num_delim:
         #not enough numbers
-    #    print 'Error on line ' + str(line_num)
-    #    thisline = infile.readline()
-    #    print thisline
+        print 'Error on line ' + str(line_num)
+        print line
 
     line_num = line_num + 1
 
